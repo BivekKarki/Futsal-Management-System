f670288c946c1b35ddfa0cae5e15e384af6c10c5
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2024-01-02 11:10:51 +0545

    create home app

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from home.views import homeview
+
+app_name = "home"
+
+urlpatterns = [
+    path('', homeview, name='homepage'),
+]
